b877826b3ae25412889a7414a80774f344483eab
Dayvid Kelly <dayvid.kelly.dk@gmail.com>
2021-09-07 11:13:34 +0400

    added for loop

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-console.log("Hello World");
-console.log("this is a dev feature");
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  console.log(element);
+}
